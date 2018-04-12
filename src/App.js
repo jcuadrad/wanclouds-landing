8bf9d091628779152e816d3cb3b6bc2b3ec2f7bc
Juan Cuadra <jcb8793@gmail.com>
2018-04-12 11:28:40 -0600

    Finish intial styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Background from './assets/BG-LANDINGPAGE-WANCLOUDS.jpg';
+import Logo from './assets/LOGO-LANDINGPAGE-WANCLOUDS.png';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      showForm: false
+    }
+  }
+
+  toggleForm = () => {
+    this.setState({
+      showForm: !this.state.showForm
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        { this.state.showForm ? <div className="dim"></div> : null }
+        <div className="App">
+          { this.state.showForm ?
+          <div className="form">
+            <div className="close" onClick={this.toggleForm}>X</div>
+            <form action="submit">
+              <label>Full Name</label>
+              <input type="text"/>
+              <label>Email</label>
+              <input type="text"/>
+              <label>Message</label>
+              <textarea name="message" id="" cols="30" rows="8"></textarea>
+              <button>SEND</button>
+            </form>
+          </div> 
+          : <div className="welcome">
+            <img src={Logo} alt="Logo" className="logo"/>
+            <h1>COMING <br/> SOON</h1>
+            <span className="button" onClick={this.toggleForm}>Contact Us</span>
+          </div> }
+        </div>
       </div>
     );
   }
